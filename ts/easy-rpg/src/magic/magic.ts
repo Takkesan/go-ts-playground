cef206585cf3a72b4d8df85ff6b390c7e2c464e6
Takkesan <95965886+Takkesan@users.noreply.github.com>
2025-06-29 01:26:24 +0000

    マジックに消費MPを追加

--- ts/easy-rpg/src/magic/magic.ts
+++ ts/easy-rpg/src/magic/magic.ts
@@ -3,34 +3,41 @@ export abstract class Magic {
   name: string;
   value: string;
   description: string;
+  mpCost: number;
 
-  constructor(name: string, value: string, description: string) {
+  constructor(
+    name: string,
+    value: string,
+    description: string,
+    mpCost: number
+  ) {
     this.name = name;
     this.value = value;
     this.description = description;
+    this.mpCost = mpCost;
   }
 }
 
 export class Agi extends Magic {
   constructor() {
-    super("アギ", "agi", "敵1体に火炎属性で小ダメージ。");
+    super("アギ", "agi", "敵1体に火炎属性で小ダメージ。", 3);
   }
 }
 
 export class Buf extends Magic {
   constructor() {
-    super("ブフ", "buf", "敵1体に氷結属性で小ダメージ。");
+    super("ブフ", "buf", "敵1体に氷結属性で小ダメージ。", 3);
   }
 }
 
 export class Gar extends Magic {
   constructor() {
-    super("ガル", "gar", "敵1体に疾風属性で小ダメージ。");
+    super("ガル", "gar", "敵1体に疾風属性で小ダメージ。", 3);
   }
 }
 
 export class Dian extends Magic {
   constructor() {
-    super("ディア", "dian", "自分のHPを小回復する。");
+    super("ディア", "dian", "自分のHPを小回復する。", 4);
   }
 }
